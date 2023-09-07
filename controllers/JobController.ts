2b4b59b38a4723af1edec3641fd61e6f3d56edcb
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-07 10:51:19 +0100

    get all jobs endpoint

--- controllers/JobController.ts
+++ controllers/JobController.ts
@@ -1,9 +1,13 @@
 import Job from "../models/JobMode"
-import {Request, Response } from "express"
+import { Request, Response } from "express"
 
 export const createJob = async (req: Request, res: Response) => {
-    const {company, position } = req.body
+    const { company, position } = req.body
     const jobs = await Job.create({ company, position })
-    res.status(201).json({jobs})
+    res.status(201).json({ jobs })
+}
+export const getAllJobs = async (req: Request, res:Response) => {
+    const jobs = await Job.find({});
+    res.status(200).json({jobs})
 }
 
--- routes/JobRouter.ts
+++ routes/JobRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 const router = Router();
 import { createJob } from "../controllers/JobController";
+import { getAllJobs } from "../controllers/JobController";
 
-router.route("/").post(createJob)
+router.route("/").post(createJob).get(getAllJobs)
 
 export default router
