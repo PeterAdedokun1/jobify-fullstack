922f911349dc7ff93d2a06862af419655bb4d510
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-07 11:02:55 +0100

    delete job endpoint

--- controllers/JobController.ts
+++ controllers/JobController.ts
@@ -6,8 +6,26 @@ export const createJob = async (req: Request, res: Response) => {
     const jobs = await Job.create({ company, position })
     res.status(201).json({ jobs })
 }
-export const getAllJobs = async (req: Request, res:Response) => {
+export const getAllJobs = async (req: Request, res: Response) => {
     const jobs = await Job.find({});
-    res.status(200).json({jobs})
+    res.status(200).json({ jobs })
 }
+export const getJob = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const job = await Job.findById(id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` })
+    }
+    return res.status(200).json({ job })
+}
+export const deleteJob = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const removeJob = await Job.findByIdAndDelete(id);
+    if (!removeJob) {
+        return res.status(404).json({ msg: `no job with id ${id}` })
+    }
+    res.status(200).json({ msg: "job deleted", removeJob })
+}
+export const UpdateJob = async (req: Request, res: Response) => {
 
+}
--- routes/JobRouter.ts
+++ routes/JobRouter.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = Router();
 import { createJob } from "../controllers/JobController";
 import { getAllJobs } from "../controllers/JobController";
-
+import { getJob } from "../controllers/JobController";
+import { deleteJob } from "../controllers/JobController";
 router.route("/").post(createJob).get(getAllJobs)
+router.route("/:id").get(getJob).delete(deleteJob)
 
 export default router
