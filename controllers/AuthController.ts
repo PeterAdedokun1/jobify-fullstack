750ef95c808acf89fbe4beed0dc2e54f40b5e158
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-07 21:29:13 +0100

    login

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -2,15 +2,15 @@ import { Request, Response } from "express"
 import User from "../models/UserModel"
 import { StatusCodes } from "http-status-codes"
 import bcrypt from 'bcryptjs';
-
+import { hashPassword } from "../utils/passwordUtils";
+// import 
 export const register = async (req: Request, res: Response) => {
     const isFirstAccount = (await User.countDocuments()) === 0;
     req.body.role = isFirstAccount ? 'admin' : 'user';
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+   const hashedPassword = await hashPassword(req.body.password)
     req.body.password = hashedPassword;
     const user = await User.create(req.body);
-    res.status(StatusCodes.OK).json({ user })
+    res.status(StatusCodes.OK).json({msg: "user created"})
 }
 export const login = async (req: Request, res: Response) => {
     res.send("login")
