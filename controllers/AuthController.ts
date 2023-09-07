3680e8beee8f915ab888cf7541b0a295b451fd0b
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-07 22:20:12 +0100

    login

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -4,14 +4,14 @@ import { StatusCodes } from "http-status-codes"
 import bcrypt from 'bcryptjs';
 import { comparePassword, hashPassword } from "../utils/passwordUtils";
 import { UnauthenticatedError } from "../errors/CustomError";
-// import 
+import { createJWT } from "../utils/tokenUtils";
 export const register = async (req: Request, res: Response) => {
     const isFirstAccount = (await User.countDocuments()) === 0;
     req.body.role = isFirstAccount ? 'admin' : 'user';
-   const hashedPassword = await hashPassword(req.body.password)
+    const hashedPassword = await hashPassword(req.body.password)
     req.body.password = hashedPassword;
     const user = await User.create(req.body);
-    res.status(StatusCodes.OK).json({msg: "user created"})
+    res.status(StatusCodes.OK).json({ msg: "user created" })
 }
 export const login = async (req: Request, res: Response) => {
     const user = await User.findOne({ email: req.body.email });
@@ -20,5 +20,7 @@ export const login = async (req: Request, res: Response) => {
     if (!isValidUser) {
         throw new UnauthenticatedError("invalid credentials");
     }
-    res.send("successfully")
+
+    const token = createJWT({ userId: user._id, role: user.role })
+    res.json({ token })
 }
