40423bfe695478f875a906cc334f3dd662256c25
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-09 21:41:12 +0100

    axious base url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import axios from "axios"
+import customFetch from './utils/CustomFetch.tsx'
+
+
+const data = await customFetch.get("/test");
+console.log(data.data)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ client/src/utils/CustomFetch.tsx
@@ -0,0 +1,6 @@
+import axios,{AxiosInstance} from "axios";
+
+const customFetch: AxiosInstance = axios.create({
+    baseURL: "/api/v1"
+})
+export default customFetch
