152770ac9244b981e456760cc2007c1f398e0b85
peterAdedokun1 <peteradedokun436@gmail.com>
2023-09-12 18:40:55 +0100

    profile

--- client/src/components/LogoutContainer.tsx
+++ client/src/components/LogoutContainer.tsx
@@ -1,28 +1,36 @@
 import { FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/LogoutContainer";
 import { useState, useContext } from "react";
-import {DashboardContext} from "./DashboardContext";
+import { DashboardContext } from "./DashboardContext";
 const LogoutContainer = () => {
-    const [showLogout, setShowLogout] = useState(false);
-    const contextValue = useContext(DashboardContext)
-    return (
-      <Wrapper>
+  const [showLogout, setShowLogout] = useState(false);
+  const contextValue = useContext(DashboardContext);
+  return (
+    <Wrapper>
+      <button
+        type="button"
+        className="btn logout-btn"
+        onClick={() => setShowLogout(!showLogout)}
+      >
+        {contextValue?.user?.avatar ? (
+          <img src={contextValue?.user.avatar} alt="avatar" className="img" />
+        ) : (
+          <FaUserCircle />
+        )}
+        {contextValue?.user?.name}
+        <FaCaretDown />
+      </button>
+      <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
         <button
           type="button"
-          className="btn logout-btn"
-          onClick={() => setShowLogout(!showLogout)}
+          className="dropdown-btn"
+          onClick={contextValue?.logoutUser}
         >
-          <FaUserCircle />
-          {contextValue?.user?.name}
-          <FaCaretDown />
+          logout
         </button>
-        <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
-          <button type="button" className="dropdown-btn" onClick={contextValue?.logoutUser}>
-            logout
-          </button>
-        </div>
-      </Wrapper>
-    );
+      </div>
+    </Wrapper>
+  );
 };
 
 export default LogoutContainer;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Link, Form, redirect, useNavigation } from "react-router-dom";
+import { Link, Form, redirect, useNavigation, useNavigate } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../utils/CustomFetch";
@@ -20,6 +20,20 @@ export const action = async ({ request }: any) => {
 export const Login = () => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting"
+    const navigate = useNavigate();
+    const loginDemoUser = async () => {
+      const data = {
+        email: "test@test.com",
+        password: "secret123",
+      };
+      try {
+        await customFetch.post("/auth/login", data);
+        toast.success("take a test drive");
+        navigate("/dashboard");
+      } catch (error: any) {
+        toast.error(error?.response?.data?.msg);
+      }
+    };
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -30,7 +44,7 @@ export const Login = () => {
         <button type="submit" className="btn btn-block" disabled={isSubmitting}>
           {isSubmitting ? "submitting..." : "submit"}
         </button>
-        <button type="button" className="btn btn-block">
+        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
           explore the app
         </button>
         <p>
